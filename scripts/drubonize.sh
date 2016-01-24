d07f5dc3b450073b17419dd1247690ebbe995d14
Alexander Tolstikov <atolstikov@adyax.com>
2016-01-24 16:37:12 +0500

    Changed drubonize script

--- scripts/drubonize.sh
+++ scripts/drubonize.sh
@@ -7,34 +7,24 @@ fi
 
 CWD=$(pwd)
 
-if [ ! -D 'sites' ]; then
-  cp -fR drubone/templates/sites .
-  cd docroot/sites/all
+if [ ! -f "sites/all/drush/local/aliases.drushrc.php" ]; then
+  echo "Copying drush aliases."
+  cp -fR drubone/templates/sites/* sites
 fi
 
 cd ${CWD}
 cd docroot/sites/all
-echo "Copying drush aliases."
+echo "Copying drush aliases for dev."
 cp -f drush/local.aliases.drushrc.php drush/dev.aliases.drushrc.php
 
 cd ${CWD}
 if [ ! -d 'drubone.config' ]; then
-  cp -fR drubone/templates/drubone.config .
-  cp -fR drubone/templates/git/drubone.config/. drubone.config
-  echo "Drubone.config initialized."
 fi
 
 cd ${CWD}
-if [ ! -D 'drubone.config' ]; then
-  mkdir drubone.config
-else
-  for file in drubone/templates/drubone.config/*; do
-    filename="${file##*/}" #basename
-    if [ ! -f "drubone.config/${filename}" ]; then
-      echo "${filename} copied to drubone.config"
-      cp ${file} drubone.config
-    fi
-  done
+if [ ! -d "drubone.config" ]; then
+  echo "Copying Drubone.config."
+  cp -fR drubone/templates/drubone.config .
 fi
 
 cd ${CWD}
